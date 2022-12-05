5eda205bb7112c19609a0157ddd04380ac3c39ba
Alessandro Perlo <aleperlo002@gmail.com>
2022-12-05 15:55:14 +0100

    Switched to sequences

--- day05/Main.hs
+++ day05/Main.hs
@@ -1,24 +1,23 @@
 import qualified Data.Text as T
-import qualified Data.Map as M
+import qualified Data.Sequence as S
+import Data.Foldable
 import Data.Char
 
 type Movement = (Int, Int, Int)
-type StackMap = M.Map Int T.Text
+type StackSeq = S.Seq T.Text
 
-parseStacks :: [T.Text] -> StackMap
+parseStacks :: [T.Text] -> StackSeq
 parseStacks rawStacks =
   let values = map T.concat . joinStacks $ init rawStacks
-  in M.fromList $ zip [1..length values] values
+  in S.fromList values
 
 joinStacks :: [T.Text] -> [[T.Text]]
-joinStacks (x:xs) = zipWith (:) (parseStackLine x) (joinStacks xs)
-joinStacks _ = repeat []
-
-parseStackLine :: T.Text -> [T.Text]
-parseStackLine line =
-  let splitLine = T.chunksOf 4 line
+joinStacks (x:xs) =
+  let splitLine = T.chunksOf 4 x
       dropAroundFn = (\x -> isSpace x || x == '[' || x == ']')
-  in map (T.dropAround dropAroundFn) splitLine
+      x' = map (T.dropAround dropAroundFn) splitLine
+  in zipWith (:) x' (joinStacks xs)
+joinStacks _ = repeat []
 
 parseMoveLine :: T.Text -> Movement
 parseMoveLine line =
@@ -26,26 +25,27 @@ parseMoveLine line =
       [x', y', z'] = map (read . T.unpack) [x, y, z] :: [Int]
   in (x', y', z')
 
-makeMovement :: StackMap -> Movement -> StackMap
+makeMovement :: StackSeq -> Movement -> StackSeq
 makeMovement stacks (0, _, _) = stacks
 makeMovement stacks (n, from, to) =
   let updated = makeMovement' stacks (1, from, to)
   in makeMovement updated (n-1, from, to)
 
-makeMovement' :: StackMap -> Movement -> StackMap
+makeMovement' :: StackSeq -> Movement -> StackSeq
 makeMovement' stacks (n, from, to) =
-  let crates = T.take n $ M.findWithDefault (T.pack "") from stacks
-      removed = M.adjust (T.drop n) from stacks
-  in M.adjust (T.append crates) to removed
+  let crates = T.take n $ S.index stacks (from-1)
+      removed = S.adjust (T.drop n) (from-1) stacks
+  in S.adjust (T.append crates) (to-1) removed
 
-result :: StackMap -> String
-result stacks = map T.head $ M.elems stacks
+result :: StackSeq -> [Movement] -> (StackSeq -> Movement -> StackSeq) -> String
+result stacks moves movementFn = map T.head $ toList finalConf
+  where finalConf = foldl movementFn stacks moves
 
 main :: IO ()
 main = do
   contents <- readFile "day05/input.txt"
   let [rawStacks, rawMoves] = map T.lines $ T.splitOn (T.pack "\n\n") $ T.pack contents
       (stacks, moves) = (parseStacks rawStacks, map parseMoveLine rawMoves)
-      res1 = result $ foldl makeMovement stacks moves
-      res2 = result $ foldl makeMovement' stacks moves
+      res1 = result stacks moves makeMovement
+      res2 = result stacks moves makeMovement'
   mapM_ print [res1, res2]
