3cc69fd2080d5a0d41988513b7d9ea500185a995
Alessandro Perlo <aleperlo002@gmail.com>
2022-12-11 21:09:09 +0100

    Halved execution time using arrays instead of lists

--- day11/Main.hs
+++ day11/Main.hs
@@ -4,11 +4,11 @@ import Data.List (sort)
 import Data.List.Split
 import Data.Foldable
 
-data Monkey = Monkey { items :: S.Seq [Int]
+data Monkey = Monkey { items :: S.Seq (Array Int Int)
                      , inspections :: Int
-                     , operation :: ([Int] -> [Int])
-                     , test :: ([Int] -> Int -> Int)
-                     , test' :: ([Int] -> Int -> Int)
+                     , operation :: (Array Int Int -> Array Int Int)
+                     , test :: (Array Int Int -> Int -> Int)
+                     , test' :: (Array Int Int -> Int -> Int)
                      , moduleN :: Int
                      }
 
@@ -28,8 +28,8 @@ parse content = listArray (0, l - 1) monkeyList
 parseMonkey :: String -> Int -> Monkey
 parseMonkey rawData n =
   let [rawItems, rawOp, rawTest, rawThen, rawElse] = tail . lines $ rawData
-      itemSeq = S.fromList . map (replicate n . read) .
-        splitOn "," . tail . dropWhile (':' /=) $ rawItems :: S.Seq [Int]
+      itemSeq = S.fromList . map (listArray (0, n-1) . replicate n . read) .
+        splitOn "," . tail . dropWhile (':' /=) $ rawItems :: S.Seq (Array Int Int)
       [op1, f, op2] = words . tail . dropWhile ('=' /=) $ rawOp
       operationFn = buildOperation (op1, f, op2)
       [m, a, b] = map (read . last . words) [rawTest, rawThen, rawElse] :: [Int]
@@ -37,39 +37,40 @@ parseMonkey rawData n =
       testFn' = buildTest' a b
   in Monkey itemSeq 0 operationFn testFn testFn' m
 
-buildTest :: Int -> Int -> Int -> [Int] -> Int -> Int
-buildTest m a b n i = if (head n) `mod` m == 0 then a else b
+buildTest :: Int -> Int -> Int -> Array Int Int -> Int -> Int
+buildTest m a b n i = if (n ! 0) `mod` m == 0 then a else b
 
-buildTest' :: Int -> Int -> [Int] -> Int -> Int
-buildTest' a b n i = if (n !! i) == 0 then a else b
+buildTest' :: Int -> Int -> Array Int Int -> Int -> Int
+buildTest' a b n i = if (n ! i) == 0 then a else b
 
-buildOperation :: (String, String, String) -> [Int] -> [Int]
-buildOperation ("old", "*", "old") n = map (\x -> x * x) n
-buildOperation ("old", "*", x) n = map ((read x :: Int) *) n
-buildOperation (_, _, x) n = map ((read x :: Int) +) n
+buildOperation :: (String, String, String) -> Array Int Int -> Array Int Int
+buildOperation ("old", "*", "old") n = fmap (\x -> x * x) n
+buildOperation ("old", "*", x) n = fmap ((read x :: Int) *) n
+buildOperation (_, _, x) n = fmap ((read x :: Int) +) n
 
-monkeyPop :: Monkey -> ([Int], Monkey)
+monkeyPop :: Monkey -> (Array Int Int, Monkey)
 monkeyPop (Monkey (n S.:<| rest) a b c d e) = (n, Monkey rest (a+1) b c d e)
 monkeyPop _ = error "Empty queue"
 
-monkeyEnque :: [Int] -> Monkey -> Monkey
+monkeyEnque :: Array Int Int -> Monkey -> Monkey
 monkeyEnque n (Monkey queue a b c d e) = Monkey (queue S.|> n) a b c d e
 
 monkeyPlay :: Int -> MonkeyArray -> MonkeyArray
 monkeyPlay i arr = let (n, m) = monkeyPop (arr ! i)
                        results = operation m n
-                       n' = map (\x -> x `div` 3) results
+                       n' = fmap (\x -> x `div` 3) results
                        dest = test m n' i
                        m' = monkeyEnque n' (arr ! dest)
                    in arr // [(i, m), (dest, m')]
 
-monkeyPlay' :: Int -> MonkeyArray -> MonkeyArray
-monkeyPlay' i arr = let (n, m) = monkeyPop (arr ! i)
-                        modules = toList . fmap (moduleN) $ arr
-                        results = operation m n
-                        n' = zipWith (\x y -> x `mod` y) results modules
-                        dest = test' m n' i
-                        m' = monkeyEnque n' (arr ! dest)
+monkeyPlay' :: Array Int Int -> Int -> MonkeyArray -> MonkeyArray
+monkeyPlay' modules i arr = let (n, m) = monkeyPop (arr ! i)
+                                results = operation m n
+                                b = bounds arr
+                                n' = listArray b $ zipWith (\x y -> x `mod` y)
+                                  (elems results) (elems modules)
+                                dest = test' m n' i
+                                m' = monkeyEnque n' (arr ! dest)
                     in arr // [(i, m), (dest, m')]
 
 monkeysPlayRounds :: MonkeyArray -> Int -> MonkeyPlayFn -> MonkeyArray
@@ -90,6 +91,7 @@ main :: IO ()
 main = do
   contents <- readFile "day11/input.txt"
   let monkeyArr = parse contents
+      modules = fmap (moduleN) $ monkeyArr
       res1 = monkeyBusiness $ monkeysPlayRounds monkeyArr 20 monkeyPlay
-      res2 = monkeyBusiness $ monkeysPlayRounds monkeyArr 10000 monkeyPlay'
+      res2 = monkeyBusiness $ monkeysPlayRounds monkeyArr 10000 (monkeyPlay' modules)
   mapM_ print [res1, res2]
